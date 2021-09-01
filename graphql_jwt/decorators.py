0fd55f7947dfa11821036b784d4e8f9966cd5310
Stijn <stijn1c@gmail.com>
2021-09-02 00:50:13 +0200

    Add jwt_refresh_cookie decorator

--- graphql_jwt/decorators.py
+++ graphql_jwt/decorators.py
@@ -193,6 +193,39 @@ def jwt_cookie(view_func):
     return wrapped_view
 
 
+def jwt_refresh_cookie(view_func):
+    """
+    Adaptation of the jwt_cookie decorator from graphql_jwt. The default decorator
+    sets both a jwt and refresh token cookie, the adaptation sets only the latter.
+    """
+
+    @wraps(view_func)
+    def wrapped_view(request, *args, **kwargs):
+        request.jwt_cookie = True
+        response = view_func(request, *args, **kwargs)
+
+        if hasattr(request, "jwt_refresh_token"):
+            refresh_token = request.jwt_refresh_token
+            expires = refresh_token.created + jwt_settings.JWT_REFRESH_EXPIRATION_DELTA
+
+            set_cookie(
+                response,
+                jwt_settings.JWT_REFRESH_TOKEN_COOKIE_NAME,
+                refresh_token.token,
+                expires=expires,
+            )
+
+        if hasattr(request, "delete_jwt_cookie"):
+            delete_cookie(response, jwt_settings.JWT_COOKIE_NAME)
+
+        if hasattr(request, "delete_refresh_token_cookie"):
+            delete_cookie(response, jwt_settings.JWT_REFRESH_TOKEN_COOKIE_NAME)
+
+        return response
+
+    return wrapped_view
+
+
 def ensure_token(f):
     @wraps(f)
     def wrapper(cls, root, info, token=None, *args, **kwargs):
